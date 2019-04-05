e58452ae80c408fc23bd4ac93b612eff21c0906d
korynewton <korynewtondev@gmail.com>
2019-04-05 08:40:07 -0700

    registration implemented

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
+const bcrypt = require('bcryptjs');
+
 const axios = require('axios');
+
+const db = require('../database/dbConfig')
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,8 +12,22 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
+async function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  try {
+    const [id] = await db('users').insert(user)
+    const addedUser = await db('users').where({ id })
+    if (id) {
+      res.status(200).json(addedUser)
+    } else {
+      res.status(500).json({ message: "Error in adding user to database" })
+    }
+  } catch(error) {
+    res.status(400).json({ message: "user already registered, try logging in" })
+  }
 }
 
 function login(req, res) {
